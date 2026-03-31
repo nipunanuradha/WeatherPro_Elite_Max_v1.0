9184a310715fea35e61cc66da3bf5a6c87e3b340
A.R.A.N Athukorala <135397733+nipunanuradha@users.noreply.github.com>
2026-04-01 04:09:59 +0530

    chore: add Android build artifacts and Gradle wrapper files

---
+++ weather app/www/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
